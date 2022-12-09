c00a3edaf0512f3155a21c77ea1f32fc09d3a548
valid <58643775+InvalidSoul@users.noreply.github.com>
2022-12-09 13:36:32 -0500

    Update 1.2.0
    - Made capitalization in answers not matter
    - Fixed some final results displaying
    - Formatted code a bit better

--- main.py
+++ main.py
@@ -11,42 +11,50 @@ import random
 import colorama
 
 problems = {
-    "What is the blue thing above you outside called?":"sky",
-    "What has 8 legs and red triangle on back?":"black widow spider"
+    "What is significant digits of 33.831":"33.8",
+    "What law states that the velocity of an object does not change unless the object is acted upon by an external force?":"first law motion",
 }
 
-spec_chars = ['!','.','?',',',';',':',"'",'/']
+spec_chars = ["!", ".", "?", ",", ";", ":", "'", "/"]
 # REMOVING SPECIAL CHARACTERS
 for spec in spec_chars:
     for question, answer in problems.items():
         if spec in answer:
-            problems[question] = answer.replace(spec, '').lower()
+            problems[question] = answer.replace(spec, "").lower()
 # TURNING ALL ANSWERS INTO LIST
 for question, answer in problems.items():
     problems[question] = answer.split()
-grade_display = len(problems); max_display = len(problems)
+grade_display = len(problems)
+max_display = len(problems)
+
 def give_question():
-    global grade_display; global max_display
+    global grade_display
+    global max_display
     if len(problems) == 0:
-        os.system('clear')
-        print(f'Congratulations You Scored {grade_display}/{max_display}\n\n%: {(100/max_display)*grade_display}%')
+        os.system("clear")
+        print(
+            f"Congratulations You Scored {grade_display}/{max_display}\n\n%: {(100/max_display)*grade_display}%"
+        )
     else:
-        os.system('clear')
+        os.system("clear")
         question, answer = random.choice(list(problems.items()))
-        finalAnswer = (input(f'\n\n{colorama.Fore.GREEN}{question}{colorama.Style.RESET_ALL}\n\n{colorama.Fore.BLUE}Answer:{colorama.Style.RESET_ALL} '))
+        finalAnswer = input(
+            f"\n\n{colorama.Fore.GREEN}{question}{colorama.Style.RESET_ALL}\n\n{colorama.Fore.BLUE}Answer:{colorama.Style.RESET_ALL} "
+        )
         for spec in spec_chars:
             for char in finalAnswer:
                 if spec in char:
-                    finalAnswer = finalAnswer.replace(spec, '').lower()
-        finalAnswer = finalAnswer.split()
-        is_right = all(x in finalAnswer for x in answer)
+                    finalAnswer = finalAnswer.replace(spec, "")
+        finalAnswer = list(str(finalAnswer.split()).lower())
+        is_right = all(x in finalAnswer for x in str(answer).lower())
         if is_right == True:
-            print('\n\nCorrect Answer!')
+            print("\n\nCorrect Answer!")
             problems.pop(question)
         else:
-            print(f'\n\nIncorrect Answers\n\nCorrect Answer: {answer}')
+            print(f"\n\nIncorrect Answers\n\nCorrect Answer: {answer}")
             problems.pop(question)
             grade_display -= 1
-        input('Press ENTER For Next Question: ')  
+        input("Press ENTER For Next Question: ")
         give_question()
+
 give_question()
