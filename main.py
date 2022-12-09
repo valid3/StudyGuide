3a1b35e4d64221d0a18aa992dbc59bbaafc9cca0
valid <58643775+InvalidSoul@users.noreply.github.com>
2022-12-09 13:11:51 -0500

    Version 1.1.0
    - Added color to the text upon showing question. 
    - New output technique.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import random
-import os
-
 # READ
 
 # Use key words as value of dictionary key. For example, setting value to sky can make you type in "The big blue sky!" and be considered right!
 
+import os
+try:
+    import colorama
+except:
+    os.system('pip install colorama')
+import random
+import colorama
+
 problems = {
     "What is the blue thing above you outside called?":"sky",
-    "What has 8 legs and red triangle on back?":"black widow spider",
+    "What has 8 legs and red triangle on back?":"black widow spider"
 }
+
 spec_chars = ['!','.','?',',',';',':',"'",'/']
-# REMOVING SPECIAL CHARS
+# REMOVING SPECIAL CHARACTERS
 for spec in spec_chars:
     for question, answer in problems.items():
         if spec in answer:
@@ -18,18 +24,16 @@ for spec in spec_chars:
 # TURNING ALL ANSWERS INTO LIST
 for question, answer in problems.items():
     problems[question] = answer.split()
-grade_display = len(problems)
-max_display = len(problems)
+grade_display = len(problems); max_display = len(problems)
 def give_question():
     global grade_display; global max_display
     if len(problems) == 0:
         os.system('clear')
-        print(f'Congratulations You Scored {max_display}/{grade_display}\n\n%: {(100/max_display)*grade_display}%')
+        print(f'Congratulations You Scored {grade_display}/{max_display}\n\n%: {(100/max_display)*grade_display}%')
     else:
         os.system('clear')
         question, answer = random.choice(list(problems.items()))
-        print(f'\n\n{question}\n\n')
-        finalAnswer = (str(input('Answer: ')))
+        finalAnswer = (input(f'\n\n{colorama.Fore.GREEN}{question}{colorama.Style.RESET_ALL}\n\n{colorama.Fore.BLUE}Answer:{colorama.Style.RESET_ALL} '))
         for spec in spec_chars:
             for char in finalAnswer:
                 if spec in char:
