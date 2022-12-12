38a1be764b02912197a80f0259ebb8739bb9dc88
valid <58643775+InvalidSoul@users.noreply.github.com>
2022-12-12 12:20:36 -0500

    Update 1.3.0
    - Formatting Changes
    - Works for Mac/Linux/Windows Now
    - Probably some other small changes I forgot about

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-# READ
-
-# Use key words as value of dictionary key. For example, setting value to sky can make you type in "The big blue sky!" and be considered right!
-
-import os
+from os import system,path
+def clear():
+    if path.sep == '\\':
+        system('cls')
+    else:
+        system('clear')
 try:
     import colorama
 except:
-    os.system('pip install colorama')
+    system('pip install colorama')
 import random
 import colorama
 
 problems = {
-    "What is significant digits of 33.831":"33.8",
     "What law states that the velocity of an object does not change unless the object is acted upon by an external force?":"first law motion",
+    "What is energy associated with the position of an object and the forces acting upon it?":"potential energy",
 }
 
 spec_chars = ["!", ".", "?", ",", ";", ":", "'", "/"]
@@ -31,15 +32,15 @@ def give_question():
     global grade_display
     global max_display
     if len(problems) == 0:
-        os.system("clear")
+        clear()
         print(
-            f"Congratulations You Scored {grade_display}/{max_display}\n\n%: {(100/max_display)*grade_display}%"
+            f"Congratulations You Scored {colorama.Fore.GREEN}{grade_display}/{max_display}{colorama.Style.RESET_ALL}\n\n {str((100/max_display)*grade_display)[:5]}%"
         )
     else:
-        os.system("clear")
+        clear()
         question, answer = random.choice(list(problems.items()))
         finalAnswer = input(
-            f"\n\n{colorama.Fore.GREEN}{question}{colorama.Style.RESET_ALL}\n\n{colorama.Fore.BLUE}Answer:{colorama.Style.RESET_ALL} "
+            f"Problems Left: {len(problems)}\n\n{colorama.Fore.GREEN}{question}{colorama.Style.RESET_ALL}\n\n{colorama.Fore.BLUE}Answer:{colorama.Style.RESET_ALL} "
         )
         for spec in spec_chars:
             for char in finalAnswer:
@@ -47,14 +48,13 @@ def give_question():
                     finalAnswer = finalAnswer.replace(spec, "")
         finalAnswer = list(str(finalAnswer.split()).lower())
         is_right = all(x in finalAnswer for x in str(answer).lower())
-        if is_right == True:
-            print("\n\nCorrect Answer!")
+        if is_right:
+            print(f"\n{colorama.Fore.GREEN}Correct Answer!{colorama.Style.RESET_ALL}")
             problems.pop(question)
         else:
-            print(f"\n\nIncorrect Answers\n\nCorrect Answer: {answer}")
+            print(f"\n\n{colorama.Fore.RED}Incorrect Answers{colorama.Style.RESET_ALL}\n\nCorrect Answer: {answer}")
             problems.pop(question)
             grade_display -= 1
         input("Press ENTER For Next Question: ")
         give_question()
-
 give_question()
