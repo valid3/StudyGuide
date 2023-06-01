0ad7fa20593a44f62b270d8cf1090b93c04c64bd
valid <58643775+InvalidSoul@users.noreply.github.com>
2023-05-31 21:15:12 -0400

    Update main.py
    - Added in setup.py Integration 
    - Small Changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# Hey, thank you for using this, I doubt anyone will, but that's okay, sometimes people underappreciate simple softwares
+# Hey, thank you for using this, I doubt anyone will, but that's okay, sometimes the least known software you find is the best
 import random
+import json
 import sys
 import os
 
@@ -11,9 +12,29 @@ else:
         os.system('clear')
 # This was made/tested on Mac and Windows. I don't imagine someone studying on a Linux (Lol)
 
-problems = {"What is nine plus ten?": {"ANSWERS": "TWENTY ONE!"},
-            "What is inf + inf": {"ANSWERS": "16"}
-            }
+problems = {}
+
+with open('guides.json', "r") as json_file:
+    data = json.load(json_file)
+if data:
+    def p():
+        clear()
+        print('Type name of guide below to study!\n')
+        for name in data.keys():
+            print(name)
+        nameInput = input('\nName Here: ')
+        if nameInput in data:
+            global problems
+            for k, v in data[nameInput].items():
+                problems[k] = {"ANSWERS" : v}
+        else:
+            print('No guide found! Please correct spelling, it is case sensitive!')
+            input('Press ENTER if you understand.')
+            p()
+    p()
+else:
+    print('You have no guides setup!')
+    sys.exit()
 
 # FORMAT
 # {Question: {Answer: [Answer] }, {Misspellings: {word: [misspellings]} }}
@@ -59,7 +80,7 @@ for ques, ans in problems.items():
 def main():
     clear()
     global current_Grade
-    if len(problems) != 0:
+    if problems:
         print(f'Problems Left: {len(problems)}\n')
         question, answer = random.choice(list(problems.items()))
         user_answer = input(f'{question}\n\n')
